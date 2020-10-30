a36eb5bcf0754fc911f947bb7417284de69267fc
125073710 <125073710@qq.com>
2020-10-30 10:02:21 +0800

    add pingyin

---
+++ PinyinIme/src/com/android/inputmethod/pinyinime/utils/LogUtil.java
@@ -0,0 +1,5 @@
+package com.android.inputmethod.pinyinime.utils;
+
+public class LogUtil {
+
+}
---
+++ PinyinIme/src/com/android/inputmethod/pinyinime/utils/LogUtil2.java
@@ -0,0 +1,22 @@
+﻿package com.android.inputmethod.pinyinime.utils;
+
+import android.util.Log;
+
+public class LogUtil2 {
+
+	private static boolean DEBUG = true;
+
+	public static void log(String tag, String msg) {
+		log(tag, msg, null);
+	}
+
+	public static void log(String tag, String msg, Throwable e) {
+		if (DEBUG) {
+			if (e != null) {
+				Log.e(tag, msg, e);
+			} else {
+				Log.i(tag, msg);
+			}
+		}
+	}
+}
